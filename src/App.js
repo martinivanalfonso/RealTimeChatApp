a3c27aec59f625c51027c6f68c254c955aec80d0
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-10 20:54:16 -0300

    added join inputs and styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Join from './components/join/join.component'
+import Chat from './components/chat/chat.component'
+
+const App = () => (
+    <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" exact component={Chat} />
+    </Router>
+)
+
+export default App
