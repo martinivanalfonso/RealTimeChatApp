d48a3792c3933f124a2ddddb5431299705746a48
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-12 13:18:31 -0300

    refactoring chat component hooks

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
-import InfoBar from '../info-bar/info-bar.component'
-import Input from '../input/input.component'
-import Messages from '../messages/messages.component'
+import InfoBar from "../info-bar/info-bar.component";
+import Input from "../input/input.component";
+import Messages from "../messages/messages.component";
 
-import './chat.styles.css'
+import "./chat.styles.css";
 
 let socket;
 
@@ -19,46 +19,44 @@ const Chat = () => {
   const END_POINT = "http://localhost:5000/";
   useEffect(() => {
     const { name, room } = queryString.parse(window.location.search);
-    console.log(name, room)
+    console.log(name, room);
     socket = io(END_POINT);
 
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room }, () => {
-
-    });
+    socket.emit("join", { name, room }, () => {});
 
     return () => {
-     
+      socket.disconnect()
     };
-  }, [END_POINT, window.location.search]);
+  }, []);
 
   useEffect(() => {
     socket.on("message", (message) => {
-      setMessages([...messages, message]);
+      setMessages(messages => [...messages, message]);
     });
-  }, [messages]);
+  }, []);
 
-  const sendMessage = e => {
-    e.preventDefault()
+  const sendMessage = (e) => {
+    e.preventDefault();
 
-      if (message) {
-          socket.emit('sendMessage', message, () => setMessage(''))
-      }
-  }
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
 
   const seeUsersOnline = () => {
-    socket.emit('seeUsersOnline', {name, room}, () => console.log('success') )
-  }
+    socket.emit("seeUsersOnline", { name, room }, () => console.log("success"));
+  };
 
-  console.log(message, messages)
+  console.log(message, messages);
 
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} seeUsersOnline={seeUsersOnline} />
-          <Messages messages={messages} name={name}/>
+        <InfoBar room={room} seeUsersOnline={seeUsersOnline} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
